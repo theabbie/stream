fd0fe12c2683012afdbc6be4341209b42ebe8109
abhishek <abhishek7gg7@gmail.com>
2019-10-08 20:02:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 var app = require('express')();
+const chrome = require('chrome-aws-lambda');
+const puppeteer = require('puppeteer-core');
 
-app.get("/*", function(req,res) {
-
+app.get("/*", async function(req,res) {
+    const browser = await puppeteer.launch({
+        args: chrome.args,
+        executablePath: await chrome.executablePath,
+        headless: chrome.headless,
+    });
+    const page = await browser.newPage();
+    await page.goto(req.query.url);
+    const file = await page.screenshot({fullPage: true});
+    await browser.close();
+    res.type("image/png").end(file);
 })
 
 app.listen(process.env.PORT);
