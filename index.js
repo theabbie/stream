5bad1c72e03323283b53d58ad1108bf6bc0778fe
abhishek <abhishek7gg7@gmail.com>
2019-10-12 22:38:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,15 @@ app.get("/*", async function(req,res) {
         headless: chrome.headless,
     });
     const page = await browser.newPage();
+    await page.setUserAgent("Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1");
+    await page.setViewport({
+      'width': 375,
+      'height': 812,
+      'deviceScaleFactor': 3,
+      'isMobile': true,
+      'hasTouch': true,
+      'isLandscape': false
+    });
     await page.goto(req.query.url);
     const file = await page.screenshot({fullPage: true});
     await browser.close();
