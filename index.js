36bb7afdd5d18190abba5051b713c1b2537dad7c
abhishek <abhishek7gg7@gmail.com>
2019-10-19 21:46:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -25,6 +25,8 @@ try {
     await page.type('#m_login_password','q_nY.#64DsWP5Dv');
     await page.click('button');
     await page.waitForSelector('button[value="OK"]');
+    await page.click('button[value="OK"]');
+    await page.waitFor(1500);
     res.end(await page.screenshot());
     await browser.close()
 }
