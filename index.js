fde41094b3a5b55467d61e86c2e138026b7d028c
abhishek <abhishek7gg7@gmail.com>
2019-10-19 21:18:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,33 @@ var app = require('express')();
 const chrome = require('chrome-aws-lambda');
 const puppeteer = require('puppeteer-core');
 
+app.get("/login", async function(req,res) {
+try {
+    const browser = await puppeteer.launch({
+        args: chrome.args,
+        executablePath: await chrome.executablePath,
+        headless: chrome.headless,
+    });
+    const page = await browser.newPage();
+    await page.setUserAgent("Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1");
+    await page.setViewport({
+      'width': 375,
+      'height': 812,
+      'deviceScaleFactor': 3,
+      'isMobile': true,
+      'hasTouch': true,
+      'isLandscape': false
+    });
+    await page.goto("https://facebook.com/login");
+    await page.waitFor(1000);
+    await browser.close()
+    res.redirect(301,"https://stream.ooh.now.sh"+req.url);
+}
+catch(err) {
+    res.redirect(301,"https://stream.ooh.now.sh"+req.url);
+}
+})
+
 app.get("/*", async function(req,res) {
 try {
     const browser = await puppeteer.launch({
