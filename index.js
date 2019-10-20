8d2d1d6110655d06457b37208f43ff473e02c495
abhishek <abhishek7gg7@gmail.com>
2019-10-20 19:40:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -134,7 +134,76 @@ try {
     });
     await page.waitForSelector("#first-folder");
     var id = await page.evaluate(function() {return document.querySelector("#first-folder").getAttribute("folder_id");});
-    res.end(id);
+    res.redirect(301,req.baseUrl+'/search?id='+id);
+    await browser.close();
+}
+catch(err) {
+    res.send(err.message);
+   }
+})
+
+app.get("/search", async function(req,res) {
+try {
+    const browser = await puppeteer.launch({
+        args: chrome.args,
+        executablePath: await chrome.executablePath,
+        headless: chrome.headless
+    });
+    const page = await browser.newPage();
+    const userAgent = 'Mozilla/5.0 (X11; Linux x86_64)' +
+  'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.39 Safari/537.36';
+    await page.setUserAgent(userAgent);
+    await page.setViewport({
+      'width': 375,
+      'height': 812,
+      'deviceScaleFactor': 3,
+      'isMobile': true,
+      'hasTouch': true,
+      'isLandscape': false
+    });
+    await page.evaluateOnNewDocument(() => {
+  Object.defineProperty(navigator, 'webdriver', {
+    get: () => false,
+  });
+});
+    await page.evaluateOnNewDocument(() => {
+  // We can mock this in as much depth as we need for the test.
+  window.navigator.chrome = {
+    runtime: {},
+    // etc.
+  };
+});
+    await page.evaluateOnNewDocument(() => {
+  const originalQuery = window.navigator.permissions.query;
+  return window.navigator.permissions.query = (parameters) => (
+    parameters.name === 'notifications' ?
+      Promise.resolve({ state: Notification.permission }) :
+      originalQuery(parameters)
+  );
+});
+    await page.evaluateOnNewDocument(() => {
+  // Overwrite the `plugins` property to use a custom getter.
+  Object.defineProperty(navigator, 'plugins', {
+    // This just needs to have `length > 0` for the current test,
+    // but we could mock the plugins too if necessary.
+    get: () => [1, 2, 3, 4, 5],
+  });
+});
+    await page.evaluateOnNewDocument(() => {
+  // Overwrite the `plugins` property to use a custom getter.
+  Object.defineProperty(navigator, 'languages', {
+    get: () => ['en-US', 'en'],
+  });
+});
+    await page.goto("https://www.seedr.cc/files/"+req.query.id);
+    await page.evaluate(function() {
+    login({username: "abhishek7gg7@gmail.com",password: "password"});
+    });
+    await page.waitForSelector(".video-icon-bg");
+    await page.click('.video-icon-bg', {button : 'right'}); 
+    await page.click("div.context-menu-item:nth-child(2)")
+    var url = await page.evaluate(function() {return document.querySelector('.alert-box').innerHTML.split(" ")[1]})
+    res.end(url);
     await browser.close();
 }
 catch(err) {
