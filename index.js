709d563fe995f88e4cba28fdb2526e9493a9bb03
abhishek <abhishek7gg7@gmail.com>
2019-10-20 18:35:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,9 @@ try {
         headless: chrome.headless
     });
     const page = await browser.newPage();
-    await page.setUserAgent("Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1");
+    const userAgent = 'Mozilla/5.0 (X11; Linux x86_64)' +
+  'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.39 Safari/537.36';
+    await page.setUserAgent(userAgent);
     await page.setViewport({
       'width': 375,
       'height': 812,
@@ -22,11 +24,45 @@ try {
       'hasTouch': true,
       'isLandscape': false
     });
+    await page.evaluateOnNewDocument(() => {
+  Object.defineProperty(navigator, 'webdriver', {
+    get: () => false,
+  });
+});
+    await page.evaluateOnNewDocument(() => {
+  // We can mock this in as much depth as we need for the test.
+  window.navigator.chrome = {
+    runtime: {},
+    // etc.
+  };
+});
+    await page.evaluateOnNewDocument(() => {
+  const originalQuery = window.navigator.permissions.query;
+  return window.navigator.permissions.query = (parameters) => (
+    parameters.name === 'notifications' ?
+      Promise.resolve({ state: Notification.permission }) :
+      originalQuery(parameters)
+  );
+});
+    await page.evaluateOnNewDocument(() => {
+  // Overwrite the `plugins` property to use a custom getter.
+  Object.defineProperty(navigator, 'plugins', {
+    // This just needs to have `length > 0` for the current test,
+    // but we could mock the plugins too if necessary.
+    get: () => [1, 2, 3, 4, 5],
+  });
+});
+    await page.evaluateOnNewDocument(() => {
+  // Overwrite the `plugins` property to use a custom getter.
+  Object.defineProperty(navigator, 'languages', {
+    get: () => ['en-US', 'en'],
+  });
+});
     await page.goto("https://www.seedr.cc/");
     await page.evaluate(function() {
     login({username: "abhishek7gg7@gmail.com",password: "password"});
     });
-    await page.waitFor(4000);
+    await page.waitFor(3000);
     res.end(await page.screenshot());
     await browser.close();
 }
