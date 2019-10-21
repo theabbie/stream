5881e2281b6a1fb362fe3624654d3d4204287c54
abhishek <abhishek7gg7@gmail.com>
2019-10-21 09:26:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -66,12 +66,12 @@ try {
     login({username: "abhishek7gg7@gmail.com",password: "password"});
     });
     await page.waitForSelector("#first-folder");
+    await page.click("#first-folder");
+    await page.keyboard.press('Delete');
     var m = req.query.m || "magnet:?xt=urn:btih:dbf21fc9a28d7c292b5cd9462683a1e150d4e0e3";
     await page.evaluate(function(m) {
     add_link(m)
     },m);
-    await page.click("#first-folder");
-    await page.keyboard.press('Delete');
     res.redirect(301,req.baseUrl+'/get');
     await browser.close();
 }
