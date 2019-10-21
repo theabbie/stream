5fde8b8f6219d1ae8aaa7631f84a43ce05f7da2f
abhishek <abhishek7gg7@gmail.com>
2019-10-21 16:40:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -217,4 +217,136 @@ catch(err) {
    }
 })
 
+app.get("/test", async function(req,res) {
+try {
+    const browser = await puppeteer.launch({
+        args: chrome.args,
+        executablePath: await chrome.executablePath,
+        headless: chrome.headless,
+        userDataDir: '/tmp/data'
+    });
+    const page = await browser.newPage();
+    const userAgent = 'Mozilla/5.0 (X11; Linux x86_64)' +
+  'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.39 Safari/537.36';
+    await page.setUserAgent(userAgent);
+    await page.setViewport({
+      'width': 375,
+      'height': 812,
+      'deviceScaleFactor': 3,
+      'isMobile': true,
+      'hasTouch': true,
+      'isLandscape': false
+    });
+    await page.evaluateOnNewDocument(() => {
+  Object.defineProperty(navigator, 'webdriver', {
+    get: () => false,
+  });
+});
+    await page.evaluateOnNewDocument(() => {
+  // We can mock this in as much depth as we need for the test.
+  window.navigator.chrome = {
+    runtime: {},
+    // etc.
+  };
+});
+    await page.evaluateOnNewDocument(() => {
+  const originalQuery = window.navigator.permissions.query;
+  return window.navigator.permissions.query = (parameters) => (
+    parameters.name === 'notifications' ?
+      Promise.resolve({ state: Notification.permission }) :
+      originalQuery(parameters)
+  );
+});
+    await page.evaluateOnNewDocument(() => {
+  // Overwrite the `plugins` property to use a custom getter.
+  Object.defineProperty(navigator, 'plugins', {
+    // This just needs to have `length > 0` for the current test,
+    // but we could mock the plugins too if necessary.
+    get: () => [1, 2, 3, 4, 5],
+  });
+});
+    await page.evaluateOnNewDocument(() => {
+  // Overwrite the `plugins` property to use a custom getter.
+  Object.defineProperty(navigator, 'languages', {
+    get: () => ['en-US', 'en'],
+  });
+});
+    await page.goto("https://www.seedr.cc/");
+    await page.evaluate(function() {
+    login({username: "abhishek7gg7@gmail.com",password: "password"});
+    });
+    await page.waitFor(3000)
+    res.end(await page.screenshot());
+    await browser.close();
+}
+catch(err) {
+    res.send(err.message);
+   }
+})
+
+app.get("/newtest", async function(req,res) {
+try {
+    const browser = await puppeteer.launch({
+        args: chrome.args,
+        executablePath: await chrome.executablePath,
+        headless: chrome.headless,
+        userDataDir: '/tmp/data'
+    });
+    const page = await browser.newPage();
+    const userAgent = 'Mozilla/5.0 (X11; Linux x86_64)' +
+  'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.39 Safari/537.36';
+    await page.setUserAgent(userAgent);
+    await page.setViewport({
+      'width': 375,
+      'height': 812,
+      'deviceScaleFactor': 3,
+      'isMobile': true,
+      'hasTouch': true,
+      'isLandscape': false
+    });
+    await page.evaluateOnNewDocument(() => {
+  Object.defineProperty(navigator, 'webdriver', {
+    get: () => false,
+  });
+});
+    await page.evaluateOnNewDocument(() => {
+  // We can mock this in as much depth as we need for the test.
+  window.navigator.chrome = {
+    runtime: {},
+    // etc.
+  };
+});
+    await page.evaluateOnNewDocument(() => {
+  const originalQuery = window.navigator.permissions.query;
+  return window.navigator.permissions.query = (parameters) => (
+    parameters.name === 'notifications' ?
+      Promise.resolve({ state: Notification.permission }) :
+      originalQuery(parameters)
+  );
+});
+    await page.evaluateOnNewDocument(() => {
+  // Overwrite the `plugins` property to use a custom getter.
+  Object.defineProperty(navigator, 'plugins', {
+    // This just needs to have `length > 0` for the current test,
+    // but we could mock the plugins too if necessary.
+    get: () => [1, 2, 3, 4, 5],
+  });
+});
+    await page.evaluateOnNewDocument(() => {
+  // Overwrite the `plugins` property to use a custom getter.
+  Object.defineProperty(navigator, 'languages', {
+    get: () => ['en-US', 'en'],
+  });
+});
+    await page.goto("https://www.seedr.cc/");
+    await page.waitFor(3000)
+    res.end(await page.screenshot());
+    await browser.close();
+}
+catch(err) {
+    res.send(err.message);
+   }
+})
+
+
 app.listen(process.env.PORT);
